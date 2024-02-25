d64a341e0670b5051c1a53a7a14cb3eb6f5aa710
ilya <ilyamalahov038@gmail.com>
2024-02-25 15:11:37 +0300

    HomeWork №1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north;
+
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(10) PRIMARY KEY,
+	company_name text NOT NULL,
+	contact_name varchar(200) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id varchar PRIMARY KEY,
+	customer_id varchar REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(200) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,59 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+from config import DATA_EMPLOYEES, PASS, DATA_CUSTOMERS, DATA_ORDERS
+
+
+def open_data_file(data) -> list:
+    """
+    Открывает файл csv. Перебирает его и создает список из словарей.
+    Т.к. в БД в колонке employee_id должен быть тип int,
+    меняем тип в row по ключу 'employee_id'.
+    :data: путь до файла
+    :return: список из словарей
+    """
+    new_list = []
+
+    with open(data, encoding='utf-8') as file:
+        csv_file = csv.DictReader(file)
+        for row in csv_file:
+            if row.get('employee_id'):
+                row['employee_id'] = int(row['employee_id'])
+            new_list.append(row)
+    return new_list
+
+
+def add_data_to_database(data_list: list, name_table: str) -> None:
+    """
+    Отправляет информацию в БД.
+    :data_list: список из словарей, заполненный данными
+    :name_table: название таблицы, куда отправляется инфо
+    :column_count: количество столбцов в этой таблице
+    """
+
+    with psycopg2.connect(
+            host='localhost',
+            database='north',
+            user='postgres',
+            password=PASS
+    ) as conn:
+        with conn.cursor() as cur:
+            for data_info in data_list:
+                count = '%s ' * len(data_info)
+                val = tuple(data_info.values())
+                cur.execute(f"INSERT INTO {name_table} VALUES ({', '.join(count.split())})", val)
+                print(val)
+    conn.close()
+
+
+if __name__ == '__main__':
+    data_employees = open_data_file(DATA_EMPLOYEES)
+    add_data_to_database(data_employees, 'employees')
+
+    data_customers = open_data_file(DATA_CUSTOMERS)
+    add_data_to_database(data_customers, 'customers')
+
+    data_orders = open_data_file(DATA_ORDERS)
+    add_data_to_database(data_orders, 'orders')
